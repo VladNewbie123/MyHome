31c512eb836512aeeb1863ead3b34f2e158e2bdf
VladNewbie123 <112088019+VladNewbie123@users.noreply.github.com>
2023-08-31 12:19:55 +0300

    Create Untitled-1.js

---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+const myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    const mySrc = myImage.getAttribute('src');
+    if(mySrc === './img/Big_Gerb_fin.png') {
+      myImage.setAttribute ('src','./img/Lesser_Coat_of_Arms_of_Ukraine.png');
+    } else {
+      myImage.setAttribute ('src','./img/Big_Gerb_fin.png');
+    }
+}
